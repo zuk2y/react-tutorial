c5fc6aba5803e032045e73046aa8d48b99de2c9a
zuk2y <suz.liang.oji@gmail.com>
2021-06-05 14:15:52 +0900

    クラス名のミスによりCSSの適用が外れていたので修正

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 
 function Square(props) {
   return (
-    <button className="squares" onClick={props.onClick}>
+    <button className="square" onClick={props.onClick}>
       {props.value}
     </button>
   );
